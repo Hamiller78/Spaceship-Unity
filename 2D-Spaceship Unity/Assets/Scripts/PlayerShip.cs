a970a819de9df20df6f332c91e83a275eebeae73
Torben Kneesch <Kneesch@gmx.de>
2024-12-23 22:33:52 +0100

    add flame animation on thrust

--- 2D-Spaceship Unity/Assets/Scripts/PlayerShip.cs
+++ 2D-Spaceship Unity/Assets/Scripts/PlayerShip.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+    public GameObject EngineFlameAnimation; // Reference to the flame GameObject
+    private Animator _engineFlameAnimationController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        if (EngineFlameAnimation != null)
+        {
+            _engineFlameAnimationController = EngineFlameAnimation.GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float thrustInput = Input.GetAxis("Vertical");
 
+        if (thrustInput != 0f)
+        {
+            // If the player is pressing the thrust button, play the flame EngineFlameAnimation
+            EngineFlameAnimation.SetActive(true);
+        }
+        else
+        {
+            EngineFlameAnimation.SetActive(false);
+        }
     }
 }
